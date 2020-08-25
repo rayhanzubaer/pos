bad69bab6fe6fd50dfb8f78338b217a1c50c11bb
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-25 20:51:11 +0600

    Add auth support with frontend

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile');
+            $table->string('photo')->nullable();
+            $table->boolean('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
