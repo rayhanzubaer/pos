056f3351f922c49278b2cf787ed1b2f372675056
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-28 09:28:20 +0600

    Update logged in user photo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,13 +33,13 @@
         <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown user-menu">
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                    <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="user-image img-circle elevation-2" alt="User Image">
+                    <img src="{{ asset(Auth::user()->photo) }}" class="user-image img-circle elevation-2" alt="User Image">
                     <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                     <!-- User image -->
                     <li class="user-header bg-primary">
-                        <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset(Auth::user()->photo) }}" class="img-circle elevation-2" alt="User Image">
 
                         <p>
 
