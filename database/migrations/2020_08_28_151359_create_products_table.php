975a7c072dbecf7c62f1eed33c797e8307f3d236
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-28 22:13:21 +0600

    created product migration

---
+++ database/migrations/2020_08_28_151359_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('code');
+            $table->string('description');
+            $table->foreignId('category_id')->constrained();
+            $table->integer('stock');
+            $table->decimal('buying_price');
+            $table->decimal('selling_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
