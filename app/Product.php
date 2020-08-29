f3b5a4278dd70937401eb9496524f5cc3b4af540
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-29 16:17:46 +0600

    Add fillable property

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'image',
+        'code',
+        'description',
+        'category_id',
+        'stock',
+        'buying_price',
+        'selling_price',
+    ];
 }
