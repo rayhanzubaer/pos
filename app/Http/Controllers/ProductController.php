032b220264252c2d897d83a7281d8748eb5b422a
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-29 15:50:13 +0600

    Create index method of ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -14,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-
+        return view('products.index')->with('products', Product::all());
     }
 
     /**
