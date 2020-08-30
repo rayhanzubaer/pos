25d77f6dd2fbb2a878eccd69ed9296c5812232a8
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-30 10:45:25 +0600

    Create customer routes

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UserController');
 Route::resource('categories', 'CategoryController');
 Route::resource('products', 'ProductController');
+Route::resource('customers', 'CustomerController');
