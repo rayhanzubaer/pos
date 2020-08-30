4284a5ec2f400baf79cac4a0b790e594af191454
Rayhan Zubaer <zubaerrayhan14@gmail.com>
2020-08-30 11:32:05 +0600

    Create migration for customers table

---
+++ database/migrations/2020_08_30_044923_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('id_document');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->date('birthdate');
+            $table->integer('total_purchase');
+            $table->dateTime('last_purchase');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
